c323781b1d0003b170da0a12a300b19ec9c9cd15
kim-yeji <sso06030@khu.ac.kr>
2020-08-05 17:58:18 +0900

    web-scraper standard

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request,redirect,send_file
+from scraper import get_jobs
+from exporter import save_to_file
+
+app = Flask("WebScraper")
+
+db = {}
+
+@app.route("/")
+def home():
+    return render_template("potato.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    if(word):
+        word = word.lower()
+
+        existingJobs = db.get(word)
+        if(existingJobs):
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html",
+        resultsNumber = len(jobs),
+        searchingBy=word,
+        jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+        
+    except:
+        return redirect("/")
+
+    
+        
+
+app.run()
+ 
